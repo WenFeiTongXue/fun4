3f4bf8951f5545813b60e2745426fdbafe12f389
WenFeiTongXue <rubbish_Z@me.com>
2019-04-17 19:03:57 +0800

    使用APlayer播放器

--- src/App.vue
+++ src/App.vue
@@ -2,73 +2,94 @@
   <div id="app">
     <my-header></my-header>
     <router-view @listenson="showson" @listenadd="addsong"/>
-    <div id="playerbox">
-      <audio id="player" src controls Volume=".5" ref="player" @ended="nextM"></audio>  
-      <span>{{play_list.name_list[play_list.star_url]}}</span>
-      <button @click="prevM">上一首</button>
-      <button @click="nextM">下一首</button>
-    </div>
-    <ul>
-        <li v-for="(l,i) of play_list.name_list" :key="i" :class="`${i==play_list.star_url?'chose':''}`">
-          <a href="javascript:;" @click="playMusic(i)">{{l}}</a>
-        </li>
-      </ul>
+    <!-- <div id="playerbox">
+      <button @click="playerShow">隐藏</button>
+      <div :class="`${player_box.addStyle?'playerShow':''}`">
+        <audio id="player" src controls Volume=".5" ref="player" @ended="nextM"></audio>
+        <span>{{play_list[playnum].name}}</span>
+        <button @click="prevM">上一首</button>
+        <button @click="nextM">下一首</button>
+      </div>
+    </div> -->
+    <aplayer :audio="audio" :lrcType="3" fixed autoplay ref="aplayer"/>
+    <ul id="playlistbox" :class="`${play_list_box.addStyle?'boxShow':''}`">
+      <li v-for="(l,i) of play_list" :key="i" :class="`${i==playnum?'chose':''}`">
+        <a href="javascript:;" @click="playMusic(i)">{{l.name}}</a>
+        <a href="javascript:;" @click="delsong(i)" class="rt">&times;</a>
+      </li>
+    </ul>
+    <footer></footer>
   </div>
 </template>
 <script>
-import myHeader from "@/components/header.vue"
+import myHeader from "@/components/header.vue";
 export default {
-  data(){return {
-    play_list:{//播放列表详情,各列表中属性一一对应
-      url_list: [],//歌曲url列表
-      name_list:[],//歌曲名称列表
-      pic_list:[],//图片列表
-      singer_list:[],//歌手列表
-      time_list:[],//时间列表
-      star_url: 0,//起始播放歌曲下标
-    }
-  }},
-  methods:{
-    addsong(data){
+  data() {
+    return {
+      play_list: [
+        {uname:""}
+      ],
+      audio:[],
+      playnum: 0,
+      play_list_box: {
+        addStyle: false
+      },
+      player_box: {
+        addStyle: false
+      }
+    };
+  },
+  methods: {
+    playerShow(e) {
+      if (this.player_box.addStyle == false) {
+        this.player_box.addStyle = true;
+        e.target.innerHTML = "显示";
+      } else {
+        this.player_box.addStyle = false;
+        e.target.innerHTML = "隐藏";
+      }
+    },
+    delsong(i) {
+      this.play_list.splice(i, 1);
+      if (i == this.playnum) {
+        this.playMusic(i);
+      }
+    },
+    addsong(data) {
+      // console.log(this.play_list);
+        data.artist=data.singer
+        data.cover=data.pic
       console.log(data)
-      console.log(this.play_list)
-        this.play_list.url_list.unshift(data.url)
-        this.play_list.name_list.unshift(data.name)
-        this.play_list.pic_list.unshift(data.pic)
-        this.play_list.singer_list.unshift(data.singer)
-        this.play_list.time_list.unshift(data.time)
+      this.audio.unshift(data);
+      this.$refs.aplayer.switch(0)
+      // this.play_list.unshift(data);
+      // this.playnum = 0;
       // console.log(this.play_list)
-      this.playMusic();
+      // this.playMusic();
     },
-    showson(data){
-      console.log(data)
-      this.play_list.url_list=[];
-      this.play_list.name_list=[];
-      this.play_list.pic_list=[];
-      this.play_list.singer_list=[];
-      this.play_list.time_list=[];
-      this.play_list.star_url=0
-      for(let i of data.songs){
-        this.play_list.url_list.push(i.url)
-        this.play_list.name_list.push(i.name)
-        this.play_list.pic_list.push(i.pic)
-        this.play_list.singer_list.push(i.singer)
-        this.play_list.time_list.push(i.time)
+    showson(data) {
+      console.log(data);
+      this.play_list = data.songs;
+      for(var i of this.play_list){
+        i.artist=i.singer
+        i.cover=i.pic
       }
-      this.playMusic()
-    }
-    ,
-    playMusic(n) {
-      if(n){
-        this.play_list.star_url=n
-      }
-      var player = document.getElementById("player");
-      player.autoplay = true;
-      player.src = this.play_list.url_list[this.play_list.star_url];
-      console.log(this.$refs.player)
-      console.log(this.play_list.star_url)
+      this.audio=this.play_list
+      console.log(this.play_list)
+      // this.playMusic();
     },
-    
+    // playMusic(n) {
+    //   if (n) {
+    //     this.playnum = n;
+    //   }
+    //   var player = document.getElementById("player");
+    //   player.autoplay = true;
+    //   player.src = this.play_list[this.playnum].url;
+    //   console.log(this.$refs.player);
+    //   console.log(this.playnum);
+    //   this.play_list_box.addStyle = true;
+    // },
+
     // geiListId(e) {
     //   var id = e.target.dataset.listId;
     //   this.axios
@@ -91,52 +112,85 @@ export default {
     //       this.playMusic();
     //     });
     // },
-    nextM(){
-      if(this.play_list.star_url==this.play_list.url_list.length-1){
-        this.play_list.star_url=0
-      }else{
-        this.play_list.star_url++
+    nextM() {
+      if (this.playnum == this.play_list.length - 1) {
+        this.playnum = 0;
+      } else {
+        this.playnum++;
       }
       this.playMusic();
     },
-    prevM(){
-      if(this.play_list.star_url!=0){
-        this.play_list.star_url--
-        this.playMusic()
+    prevM() {
+      if (this.playnum != 0) {
+        this.playnum--;
+        this.playMusic();
       }
     }
   },
-  components:{
+  components: {
     myHeader
   }
-}
+};
 </script>
 
 <style>
-*{
-  margin:0;padding:0;
+* {
+  margin: 0;
+  padding: 0;
 }
-body{
-  font-size:14px;
-  color:#000;
+body {
+  font-size: 14px;
+  color: #000;
 }
-a{
+a {
   text-decoration: none;
-  color:#000;
+  color: #000;
 }
-.chose{
-  font-weight:bold;
+footer {
+  height: 100px;
 }
-#playerbox{
-  width:1200px;
-  background:#b7e3f3;
-  position:fixed;
+.chose {
+  font-weight: bold;
+}
+#playerbox {
+  /* width:1200px; */
+  background: #b7e3f3;
+  position: fixed;
   left: 351.5px;
-  bottom:0;
+  bottom: 0;
+  height: 56px;
+  overflow: hidden;
+  z-index: 100;
+  display: flex;
   /* opacity: 0; */
 }
-#player{
-  position:relative;
+#playerbox > div:nth-child(2) {
+  transition: all 0.3s;
 }
-
+.playerShow {
+  margin-top: 56px;
+}
+#player {
+  position: relative;
+}
+#playlistbox {
+  background: #b7e3f3;
+  width: 300px;
+  height: 500px;
+  position: fixed;
+  top: 100px;
+  right: 20px;
+  overflow: auto;
+  opacity: 0;
+  transition: all 0.3s;
+}
+#playlistbox a {
+  color: #888;
+}
+#playlistbox.boxShow {
+  opacity: 1;
+}
+.rt {
+  float: right;
+}
 </style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -4,7 +4,7 @@
       <img src="img/fw1.png" alt id="logo">
     </router-link>
     <div>
-      <el-input placeholder="请输入内容" v-model="kword" @keyup.13.native="search">
+      <el-input placeholder="请输入内容" v-model="kword" @keyup.13.native="search" width="400">
         <i slot="suffix" class="el-input__icon el-icon-search" @click="search"></i>
       </el-input>
     </div>
@@ -17,8 +17,9 @@ export default {
   },
   methods: {
     search(e) {
-      console.log(e.keyCode);
-      this.$router.push(`/search/${this.kword}`);
+      if(this.kword.trim()!==""){
+        this.$router.push(`/search/${this.kword}`);
+      }
     }
   },
   created() {
